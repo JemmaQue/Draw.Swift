dd1c6538589f2040846c711d0dec7f453e911f09
Jemma <que.jemma@gmail.com>
2019-12-05 19:24:57 +0800

    add feature move

--- Draw/Draw/DemoViewController.swift
+++ Draw/Draw/DemoViewController.swift
@@ -13,12 +13,17 @@ import RxSwift
 class DemoViewController: UIViewController {
 
     @IBOutlet weak var dynamicBox: Box!
+    @IBOutlet weak var rangeView: UIView!
     @IBOutlet weak var rectangleBox: Box!
     @IBOutlet weak var circleBox: Box!
     @IBOutlet weak var triangleBox: Box!
     @IBOutlet weak var btnRectangle: UIButton!
     @IBOutlet weak var btnCircle: UIButton!
     @IBOutlet weak var btnTriangle: UIButton!
+    var repeatTimer: Timer?
+    var moveX: CGFloat?
+    var moveY: CGFloat?
+    
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -41,6 +46,50 @@ class DemoViewController: UIViewController {
         btnTriangle.rx.tap.subscribe(onNext: { [unowned self] _ in
             self.dynamicBox.boxShape = .triangle
         }).disposed(by: disposeBag)
+        
+        let theta: Double = Double.random(in: 15...30)
+        moveX = CGFloat(cos(theta * M_PI / 180))
+        moveY = CGFloat(sin(theta * M_PI / 180))
+        start()
     }
 
+}
+
+extension DemoViewController {
+    func start() {
+        repeatTimer =
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [unowned self] _ in
+                self.move()
+        })
+    }
+    
+    func move() {
+        guard let moveX = moveX, let moveY = moveY else {
+            return
+        }
+        if (dynamicBox.frame.maxX + moveX > rangeView.frame.maxX){
+            self.moveX = moveX * -1;
+        }
+        
+        if (dynamicBox.frame.maxY + moveY > rangeView.frame.maxY){
+            self.moveY = moveY * -1;
+        }
+        
+        if (dynamicBox.frame.minX + moveX < rangeView.frame.minX){
+            self.moveX = moveX * -1;
+        }
+        
+        if (dynamicBox.frame.minY + moveY < rangeView.frame.minY){
+            self.moveY = moveY * -1;
+        }
+        
+        dynamicBox.frame = CGRect(x: dynamicBox.frame.origin.x + moveX,
+                                  y: dynamicBox.frame.origin.y + moveY,
+                                  width: dynamicBox.frame.size.width, height: dynamicBox.frame.size.height);
+    }
+    
+    func stop() {
+        repeatTimer?.invalidate()
+    }
+    
 }
